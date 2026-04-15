ce3d6c7429f959fc495b0de4e6dd3580e38816b4
meronalemayehu <mbalemayehu@gmail.com>
2026-04-15 14:58:13 -0400

    created function to interact with backend. created views.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,7 +15,10 @@ urlpatterns = [
     # path for cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
-
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
-
+    path('add_review', views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
